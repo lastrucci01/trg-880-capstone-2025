2ca3a4bff8aba2dd5ce679cc5c627c70823673b7
GabriellaGricia <u20465182@tuks.co.za>
2025-10-03 15:36:14 +0200

    Add files via upload

--- code.R
+++ code.R
@@ -6,6 +6,7 @@ library(writexl)
 library(skimr)
 library(dplyr)
 library(stringr)
+library(lubridate)
 
 data <- read_sas("C:/Users/Gab/OneDrive - University of Pretoria/Documents/TRG880/Capstone assignment/summary12025.sas7bdat")
 
@@ -17,40 +18,65 @@ write_xlsx(data, "C:/Users/Gab/OneDrive - University of Pretoria/Documents/TRG88
 skim(data)
 str(data) 
 
-table(is.na(data))
 colSums(is.na(data))
 
 
-# Convert dates
-data$COMMENCEMENT_DATE <- as.Date(data$COMMENCEMENT_DATE, format="%Y-%m-%d")
-data$STATUS_DATE <- as.Date(data$STATUS_DATE, format="%Y-%m-%d")
-data$DATE_OF_BIRTH <- as.Date(data$DATE_OF_BIRTH, format="%Y-%m-%d")
-
-unique(data$PAYMENT_MODE)
-table(unique(data$GENDER))
-unique(data$MARITAL_STATUS)
-unique(data$TITLE)
-
-summary(data[, c("PREMIUM", "INCOME", "TERM", "nloans")])
-
-# AGE check
-data$AGE <- as.numeric(difftime(Sys.Date(), data$DATE_OF_BIRTH, units = "days")) %/% 365
-summary(data$AGE)
+# ----- Duplicated data -----
 
 
-# Convert to numeric/character
-data$INCOME <- gsub("[,$ ]", "", data$INCOME)
-data$INCOME <- as.numeric(data$INCOME)
+
 
 
 # ----- Remove unwanted STATUS values -----
 data <- data %>%
-filter(!STATUS %in% c("NFS", "NUL", "PUP"))
+  filter(!STATUS %in% c("NFS", "NUL", "PUP", "MAT", "TRM"))%>%
+  mutate(
+    STATUS = str_to_upper(STATUS),
+    STATUS = case_when(
+      STATUS == "LAP" ~ "LAP",
+      STATUS == "ACT" ~ "ACT",
+      TRUE ~ STATUS))
+   
 table(data$STATUS)
 
 
+# ------ Future dates ------
+date_cols <- c("STATUS_DATE", "COMMENCEMENT_DATE", "PAYER_DATE_OF_BIRTH",
+               "INSURED_DATE_OF_BIRTH", "DATE_OF_BIRTH")
+data <- data %>%
+  mutate(across(all_of(date_cols), ~ as.Date(.)))
+future_counts <- sapply(data[date_cols], function(x) sum(x > Sys.Date(), na.rm = TRUE))
 
-# ----- Occupation code has 15194 NAS - convert strings like "NA", "N/A", "" to actual NA
+future_counts
+sum(future_counts)
+
+
+# ----- Create Payer_age col -----
+data <- data %>%
+  mutate(
+    PAYER_DATE_OF_BIRTH = as.Date(PAYER_DATE_OF_BIRTH),      
+    PAYER_AGE = year(Sys.Date()) - year(PAYER_DATE_OF_BIRTH))
+summary(data$PAYER_AGE)
+
+
+
+
+
+# ----- Convert dates ------
+data$COMMENCEMENT_DATE <- as.Date(data$COMMENCEMENT_DATE, format="%Y-%m-%d")
+data$STATUS_DATE <- as.Date(data$STATUS_DATE, format="%Y-%m-%d")
+data$DATE_OF_BIRTH <- as.Date(data$DATE_OF_BIRTH, format="%Y-%m-%d")
+
+
+# ------ Convert to numeric/character -------
+data$INCOME <- gsub("[,$ ]", "", data$INCOME)
+data$INCOME <- as.numeric(data$INCOME)
+
+
+
+# ----- Outliers and missing values ------
+
+# Occupation code - has many NAS, convert strings like "NA", "N/A", "" to actual NA
 data$OCCUPATION_CODE <- ifelse(data$OCCUPATION_CODE %in% c("NA", "N/A", ""), NA, data$OCCUPATION_CODE)
 data$OCCUPATION_CODE <- as.factor(data$OCCUPATION_CODE)
 
@@ -59,7 +85,7 @@ data <- data %>%
 table(is.na(data$OCCUPATION_CODE))
 
 
-# ------- Group occupation_desc1 ------
+# Occupation_desc1 - need to decide how to bin it
 top_jobs <- data %>%
   group_by(OCCUPATION_DESC1) %>%
   summarise(count = n()) %>%
@@ -76,6 +102,7 @@ unemployed_jobs <- data %>%
   summarise(n_policies = n()) %>%
   arrange(desc(n_policies))
 
+
 # Change to employed or unemployed
 data <- data %>%
   mutate(OCC_GROUP = case_when(
@@ -89,8 +116,6 @@ table(is.na(data$OCCUPATION_CODE))
 
 
 
-# ----- Outliers ------
-
 # Term - has very high values that are unrealistic, replace > 100 values with mode.
 hist(data$TERM, breaks = 50, main = "Policy Term Distribution", xlab = "Years")
 summary(data$TERM)
@@ -103,27 +128,31 @@ summary(data$TERM)
 
 
 
-# Payfreq has 537 counts of 147 - maybe a code for Unknown, remove 147
+# Payfreq - has counts of 147 - maybe a code for Unknown, make NA
 hist(data$PAYFREQ, breaks = 50, main = "Pay Freq Distribution", xlab = "Freq")
 summary(data$PAYFREQ)
 
 data <- data %>%
-  filter(PAYFREQ != 147)
-table(data$PAYFREQ)
+  mutate(PAYFREQ = ifelse(PAYFREQ == 147, NA, PAYFREQ))
+summary(data$PAYFREQ)
+
+
 
 
 # Premium - should have only positive values, remove - signs. Extreme values are unrealistic 
-# relative to income, term and occupation. Replace all with mean.
+# relative to income, term and occupation. Replace all with median.
 hist(data$PREMIUM, breaks = 50, main = "Premium Distribution", xlab = "Freq")
 summary(data$PREMIUM)
+sum(is.na(data$PREMIUM) | data$PREMIUM == "")
 
+premium_median <- median(data$PREMIUM[data$PREMIUM <= (mean(data$PREMIUM, na.rm = TRUE) + 4*sd(data$PREMIUM, na.rm = TRUE))], na.rm = TRUE)
 data <- data %>%
   mutate(
     PREMIUM = abs(PREMIUM),
-    PREMIUM = ifelse(PREMIUM > (mean(PREMIUM, na.rm = TRUE) + 4*sd(PREMIUM, na.rm = TRUE)),
-                     mean(PREMIUM[PREMIUM <= (mean(PREMIUM, na.rm = TRUE) + 4*sd(PREMIUM, na.rm = TRUE))], na.rm = TRUE),
-                     PREMIUM)
-  )
+    PREMIUM = ifelse(
+      PREMIUM > (mean(PREMIUM, na.rm = TRUE) + 4*sd(PREMIUM, na.rm = TRUE)),
+      premium_median,        
+      PREMIUM))
 summary(data$PREMIUM)
 
 
@@ -133,40 +162,38 @@ hist(data$INCOME, breaks = 50, main = "Income Distribution", xlab = "Freq")
 summary(data$INCOME)
 
 income_outlier <- data$INCOME > (mean(data$INCOME, na.rm = TRUE) + 4*sd(data$INCOME, na.rm = TRUE))
+income_median <- median(data$INCOME[!income_outlier & !is.na(data$INCOME)], na.rm = TRUE)
 data <- data %>%
-  mutate(INCOME = ifelse(is.na(INCOME) | income_outlier, mean(data$INCOME, na.rm = TRUE), INCOME))
+  mutate(INCOME = ifelse(is.na(INCOME) | income_outlier, income_median, INCOME))
 summary(data$INCOME)
 
 
-# Payment mode - typos are outliers, remove PLS and change Add to ADD (should only be ADD, ASO, DSO and RSO)
-hist(data$PAYMENT_MODE, breaks = 50, main = "Payment Mode Distribution", xlab = "Freq")
+# Payment mode - typos are outliers, change PLS to NA then replace with mode ? and change Add to ADD 
+# (should only be ADD, ASO, DSO and RSO)
 table(data$PAYMENT_MODE)
+sum(is.na(data$PAYMENT_MODE) | data$PAYMENT_MODE == "")
 
 data <- data %>%
-  mutate(PAYMENT_MODE = str_to_upper(PAYMENT_MODE),
-         PAYMENT_MODE = ifelse(PAYMENT_MODE == "ADD", "ADD", PAYMENT_MODE)) %>%
-  filter(PAYMENT_MODE != "PLS")
+  mutate(
+    PAYMENT_MODE = str_to_upper(PAYMENT_MODE),
+    PAYMENT_MODE = ifelse(PAYMENT_MODE == "PLS", "NA", PAYMENT_MODE)
+  )
 table(data$PAYMENT_MODE)
 
-
-# Status date - outliers are dates before company existed, or in the far future.
-
-
-
-# Commencement date -
 
 
 
 # Marital status - should only be D, M, N (unknown), S, W. Replace N and "" with Unknown and map Mrs to M for females.
-
+table(data$MARITAL_STATUS)
 data <- data %>%
-  mutate(MARITAL_STATUS = as.character(MARITAL_STATUS)) %>%
-  mutate(MARITAL_STATUS = ifelse(
-    MARITAL_STATUS %in% c("", "N") & TITLE == "MRS" & GENDER == "F",
-    "M", MARITAL_STATUS)) %>%
-  mutate(MARITAL_STATUS = ifelse(MARITAL_STATUS %in% c("", "N"), "Unknown", MARITAL_STATUS)) %>%
-  mutate(MARITAL_STATUS = factor(MARITAL_STATUS, levels = c("M","S","D","W","Unknown")))
-    
+  mutate(
+    MARITAL_STATUS = as.character(MARITAL_STATUS),
+    MARITAL_STATUS = ifelse(MARITAL_STATUS %in% c("", "N") & TITLE == "MRS" & GENDER == "F",
+                            "M", MARITAL_STATUS),
+    MARITAL_STATUS = ifelse(MARITAL_STATUS %in% c("", "N"),
+                            "NA", MARITAL_STATUS),
+    MARITAL_STATUS = factor(MARITAL_STATUS,
+                            levels = c("M","S","D","W","NA")))
 table(data$MARITAL_STATUS)
 unique(data$MARITAL_STATUS)
 
@@ -179,21 +206,30 @@ table(data$INSURED_MARITAL_STATUS)
 
 
 # Payer_dateofbirth - only ages between 18 and 100,
-
+summary(data$PAYER_DATE_OF_BIRTH)
 
 
 # Gender - typos like "MF"
+table(data$GENDER)
 
 
-# ps_lapse1 - should be 0–100%, all positive. Replace - values and outliers with mode
+# ps_lapse1 - should be 0–100%, all positive. Take out - sign, replace values > 100 with median.
 hist(data$ps_lapse1, breaks = 50, main = "Lapsed Distribution", xlab = "Freq")
 summary(data$ps_lapse1)
+
+lapse_median <- median(data$ps_lapse1[data$ps_lapse1 <= 100], na.rm = TRUE)
+data <- data %>%
+  mutate(ps_lapse1 = abs(ps_lapse1)) %>%
+  mutate(ps_lapse1 = ifelse(ps_lapse1 > 100, lapse_median, ps_lapse1))
+
+summary(data$ps_lapse1)
+
 
 
 
 # nbank - must be positive, 0-5 is realistic
 hist(data$nbank, breaks = 50, main = "No. Bank Accounts Distribution", xlab = "Freq")
-table(data$nloans)
+table(data$nbank)
 
 
 

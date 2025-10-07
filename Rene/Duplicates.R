260bea895caafa39f1a5d1875cf99bfa1ec35112
ReneGerryts <u21531626@tuks.co.za>
2025-10-07 10:21:53 +0200

    Update Duplicates.R
    Fixed missing %>% operator.
    Removed composite_key column at the end to save space.
    Cleaned up.

--- Rene/Duplicates.R
+++ Rene/Duplicates.R
@@ -1,24 +1,18 @@
-setwd("~/Masters/TRG_880/Capstone project/summary12025")
-
+library(dplyr)
 
 remove_duplicates <- function(df)
 {
   ignore_cols <- c("POLICY_NUMBER", "NBANK", "NLOANS", "EE")
   
-  df <- df %>%
-  mutate(
-    composite_key = apply(select(., -all_of(ignore_cols)), 1, paste, collapse = "_"),
-    
-  df_no_dups_composite <- df %>%
-  group_by(composite_key) %>%
-  slice_head(n = 1) %>%
-  ungroup()
-  )
+  new_df <- df %>%
+    mutate(
+      composite_key = apply(select(., -all_of(ignore_cols)), 1, paste, collapse = "_")
+    ) %>%
+    df_no_dups_composite <- df %>%
+    group_by(composite_key) %>%
+    slice_head(n = 1) %>%
+    ungroup() %>%
+    select(-composite_key)
   
-  return(df)
+  return(new_df)
 }
-
-
-#hi
-
-

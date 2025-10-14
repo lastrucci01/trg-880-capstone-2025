6552dd22859301eec9aefa7169f171c0cfdc08a0
GabriellaGricia <u20465182@tuks.co.za>
2025-10-14 09:39:18 +0200

    income and premium groups

--- Gabi/income groups.R
+++ Gabi/income groups.R
@@ -1,8 +1,13 @@
 # Income groups 
-df <- df %>%
-  mutate(INCOME_group = cut(
-    INCOME,
-    breaks = c(0, 500, 1000, 5000, 10000, 50000, Inf),
-    labels = c("0-500","500-1k","1k-5k","5k-10k","10k-50k","50k+"),
-    include.lowest = TRUE
-  ))
+income_groups <- function(df, income_col = "INCOME") {
+  df <- df %>%
+    mutate(
+      INCOME_group = cut(
+        .data[[income_col]],
+        breaks = c(0, 500, 1000, 5000, 10000, 50000, Inf),
+        labels = c("0-500","500-1k","1k-5k","5k-10k","10k-50k","50k+"),
+        include.lowest = TRUE
+      )
+    )
+  return(df)
+}
--- Gabi/premium groups.R
+++ Gabi/premium groups.R
@@ -1,8 +1,14 @@
 # Premium groups 
-df <- df %>%
-  mutate(PREMIUM_group = cut(
-    PREMIUM,
-    breaks = c(0, 100, 150, 200, 300, 500, 1000, 2000, 5000, Inf),
-    labels = c("0-100","100-150","150-200","200-300","300-500","500-1k","1k-2k","2k-5k","5k+"),
-    include.lowest = TRUE
-  ))
+premium_groups <- function(df, premium_col = "PREMIUM") {
+  df <- df %>%
+    mutate(
+      PREMIUM_group = cut(
+        .data[[premium_col]],
+        breaks = c(0, 100, 150, 200, 300, 500, 1000, 2000, 5000, Inf),
+        labels = c("0-100","100-150","150-200","200-300","300-500",
+                   "500-1k","1k-2k","2k-5k","5k+"),
+        include.lowest = TRUE
+      )
+    )
+  return(df)
+}
